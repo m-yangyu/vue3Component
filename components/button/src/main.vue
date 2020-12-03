acf8ffe6bca01e5b48942ff597e5611c029887ab
马齐翔 <maqixiang4190@100.me>
2020-12-03 11:56:27 +0800

    修改组件写法

--- components/button/src/main.vue
+++ components/button/src/main.vue
@@ -3,19 +3,11 @@
     <div>{{ buttonObject.text }}</div>
   </div>
 </template>
-<script lang="ts">
+<script setup lang="ts">
 import { reactive } from 'vue';
 
-export default {
-	name: 'Button',
-	setup() {
-		const buttonObject = reactive({
-			text: '1',
-			number: 2,
-		});
-		return {
-			buttonObject,
-		};
-	},
-};
+const buttonObject = reactive({
+	text: '1',
+	number: 2,
+});
 </script>
